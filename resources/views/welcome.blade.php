fa670224e6fd4b34fa9822050c79e91dab5c364d
andrih <95890317+andrih98s@users.noreply.github.com>
2023-03-29 12:40:50 +0300

    Add files via upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>The server is under construction</title>
+    <style>
+        body {
+            background-color: #f2f2f2;
+            font-family: Arial, sans-serif;
+            color: #333;
+        }
+        .container {
+            margin: 0 auto;
+            max-width: 600px;
+            text-align: center;
+            padding: 20px;
+            border: 1px solid #ccc;
+            background-color: #fff;
+            margin-top: 50px;
+            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            font-size: 32px;
+            margin-bottom: 20px;
+        }
+        p {
+            font-size: 18px;
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>The server is under construction</h1>
+        <p>We're sorry, but our server is currently undergoing maintenance. We'll be back soon!</p>
+       
+    </div>
+</body>
+</html>
